b8fb38d1d36350cfcbe214bc61f4407daa040c42
Roku <parkour86@gmail.com>
2025-06-23 22:08:44 -0700

    .

--- actions/PullRequests.py
+++ actions/PullRequests.py
@@ -38,7 +38,7 @@ class PullRequestsActions(ActionBase):
         else:
             log.info("GithubPlugin: Invalid token or URL")
             self.clear_labels("error")
-            self.set_media(media_path=os.path.join(self.plugin_base.PATH, "assets", "info.png"), size=0.9)
+            self.set_media(media_path=os.path.join(self.plugin_base.PATH, "assets", "#B7B700.png"), size=0.9)
             self.set_top_label("\nConfigure\nGithub\nPlugin", color=[255, 100, 100], outline_width=1, font_size=17)
 
         self.start_refresh_timer()
@@ -162,7 +162,7 @@ class PullRequestsActions(ActionBase):
                     self.set_center_label("PRs", color=[100, 255, 100], outline_width=2, font_size=20, font_family="cantarell")
                     self.set_bottom_label(f"{pr_count}", color=[100, 255, 100], outline_width=4, font_size=20, font_family="cantarell")
                     # Set default gray Github icon
-                    self.set_media(media_path=os.path.join(self.plugin_base.PATH, "assets", "#A00000.png"), size=0.9)
+                    self.set_media(media_path=os.path.join(self.plugin_base.PATH, "assets", "#236B23.png"), size=0.9)
                     # Extract SHAs and check commit statuses only if there are PRs
                     if pr_count > 0:
                         shas = [pr["head"]["sha"] for pr in pulls if "head" in pr and "sha" in pr["head"]]
